69a3fb957372d9e683174899052bc95dc6a7a255
Amey Kulkarni <kulkarni.amey1998@gmail.com>
2026-04-24 15:21:21 -0700

    Fix import statement for pathlib in dashboard.py

--- Stramlitapp/dashboard.py
+++ Stramlitapp/dashboard.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from pathlib import path
+from pathlib import Path
 
 st.set_page_config(page_title="Healthcare Financial Risk Dashboard", layout="wide")
 
